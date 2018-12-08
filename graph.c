a5917d18c657e3c292e301c6a48a2720e0aa237a
Epistam <epistam.dev@gmail.com>
2018-12-08 19:10:25 +0100

    Trying to fix y-gap filling and using abs() to make drawing conditions cleaner

--- graph.c
+++ graph.c
@@ -53,7 +53,7 @@ void drawLine(GraphPtr graph, double slope, double xOffset, double yOffset, int 
 			realHeight = slope*((1/graph->xZoom)*i+xOffset)+yOffset; // Apply x-zoom to x, add xOffset (Hz translation), then compute real y and add yOffset, 1/xZoom cause unzooming by 0.5x would mean expand what is displayed on x axis by a factor of 2
 			graphHeight = (int)round(graph->yZoom*realHeight); // Apply yZoom and round to closest integer to get graph y
 			
-			if(graphHeight < graph->ySize && graphHeight > -graph->ySize && !(!init && (i == 0 || graphHeight == 0))) { // Check if the point is within graph bounds or on axes lines
+			if(abs(graphHeight) < graph->ySize && !(!init && (i == 0 || graphHeight == 0))) { // Check if the point is within graph bounds or on axes lines
 				termGoto(i*2+graph->xSize,(-graphHeight)+graph->ySize); // Minus because y origin is the top of screen
 				drawDot(mode,15); // Draw the point
 			}
@@ -61,31 +61,33 @@ void drawLine(GraphPtr graph, double slope, double xOffset, double yOffset, int 
 		// Second pass : draw fill-up points to make lines continuous (TODO find a better way)
 		// Notice this is a thing only for straight lines. Need to find a way to assess continuity so we don't end up linking +-inf if f(x) = 1/x
 		// Arbitrarily start from the left to fill up holes 
-/*		int h1, h2;
+		int h1, h2;
 		int sign; 
-		for(i = -graph->xSize + 1 ; i < graph->xSize ; i++) {
-			h2 = (int)round(graph->yZoom*(slope*(graph->xZoom*i+xOffset)+yOffset));
-			h1 = (int)round(graph->yZoom*(slope*(graph->xZoom*(i-1)+xOffset)+yOffset));
-			// Code for mere mortals
-*//*			if(h2 - h1 > 0) { // h2 is over h1
-				for(j = h1+1 ; j < h2 ; j++) {
-					termGoto(i-1, j); 
+		for(i = -graph->xSize/2 ; i < graph->xSize/2 ; i++) {
+			h2 = (int)round(graph->yZoom*slope*((1/graph->xZoom)*i+xOffset)+yOffset); 
+			h1 = (int)round(graph->yZoom*slope*((1/graph->xZoom)*(i-1)+xOffset)+yOffset); 
+			if(abs(h2) < graph->ySize && abs(h1) < graph->ySize) {
+				// Code for mere mortals
+				if(h2 - h1 > 0) { // h2 is over h1
+					for(j = h1+1 ; j < h2 ; j++) {
+						termGoto(((i-1)*2)+graph->xSize, -(j*2)+graph->ySize); 
+						drawDot(mode, 15);
+					}
+				} else if(h2 - h1 < 0) { // h2 is over h1 
+					for(j = h1-1 ; j > h2 ; j--) {
+						termGoto(((i-1)*2)+graph->xSize, -(j*2)+graph->ySize); 
+						drawDot(mode, 15);
+					}
+				} // Nothing to fill if they're both at the same height
+
+				// Code for the Chad ternary users
+				/*	sign = ((h2-h1 > 0)? 1 : -1);
+				for(j = h1 + sign ; sign*j < sign*h2 ; j += sign) {
+					termGoto((i-1)*2, j*2); 
 					drawDot(mode, 15);
-				}
-			} else if(h2 - h1 < 0) { // h2 is over h1 
-				for(j = h1-1 ; j > h2 ; j--) {
-					termGoto(i-1, j); 
-					drawDot(mode, 15);
-				}
-			} // Nothing to fill if they're both at the same height
-*//*
-			// Code for the Chad ternary users
-			sign = ((h2-h1 > 0)? 1 : -1);
-			for(j = h1 + sign ; sign*j < sign*h2 ; j += sign) {
-				termGoto(i-1, j); 
-				drawDot(mode, 15);
+				}*/
 			}
-		}*/
+		}
 	} else {
 		int xOffsetIntegBuf = (int)round(graph->xZoom*xOffset) + graph->xSize;
 		for(int i = 0 ; i < 2*graph->ySize ; i++) {
@@ -108,8 +110,8 @@ GraphPtr initGraph() {
 	Graph *graph = malloc(sizeof(Graph));
 	graph->xSize = ws.ws_col/2;
 	graph->ySize = ws.ws_row/2;
-	graph->xZoom = 2;
-	graph->yZoom = 1;
+	graph->xZoom = 1;
+	graph->yZoom = 4;
 
 	// Draw main axes
 	drawLine(graph,0,0,0,0,0,1); // x-axis
